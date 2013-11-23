91a54ad6de7f74ebdb8d222587804d072a057829
Daniel Hertz <daniel@dhertz.com>
2013-11-23 18:07:31 +0000

    Ruby version changed date class

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require "haml"
 
 get "/" do
   dob = Date.new(1993, 8, 25)
-  now = Time.now.utc.to_date
+  now = Date.today
   @Age = now.year - dob.year -
     ((now.month > dob.month ||
   	  (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
