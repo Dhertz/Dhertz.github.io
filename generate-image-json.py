198ab76769eb46bb33c39d7b770d0fac7a480918
Daniel Hertz <daniel@dhertz.com>
2016-04-28 11:34:06 +0100

    Create list of named tuples from instagram media request

--- generate-image-json.py
+++ generate-image-json.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+from collections import namedtuple
 import requests
 import sys
-
 
 def main():
 	if len(sys.argv) != 2:
@@ -10,11 +10,18 @@ def main():
 			"the account you wish to use")
 
 	intsagram_secret = sys.argv[1]
+	get_my_media(intsagram_secret)
+
+def get_my_media(secret, max_id=""):
+	# Instagram paginates it's media, so we might have to make multiple requests
 	req = requests.get('https://api.instagram.com/v1/users/self/media/recent/',
-		params={'access_token':intsagram_secret})
-	if req.status_code == 200:
-		print req.json()
+		params={'access_token':secret, 'max_id':max_id})
+	if req.status_code == 200 and req.json()['meta']['code'] == 200:
+		Media = namedtuple('Media', 'link image id')
+		images = [Media(m['link'], m['images']['standard_resolution']['url'], m['id']) for m in req.json()['data']]
+		print images
 	else:
+		print("Instagram API request failed!")
 		req.raise_for_status()
 
 if __name__ == '__main__':
