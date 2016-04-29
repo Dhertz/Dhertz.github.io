7d0fd91ccf79a01ffb280bacc78b4a981f0ef910
Daniel Hertz <daniel@dhertz.com>
2016-04-29 01:28:10 +0100

    Fix logic in add_images, add checking file in github and commiting new images

--- generate-image-json.py
+++ generate-image-json.py
@@ -1,20 +1,57 @@
 #!/usr/bin/env python
 
-import json
-import requests
-import sys
+from argparse import ArgumentParser
+from base64 import b64encode, b64decode
+from json import dumps, loads
+from requests import get, put
 
 insta_self_recent = 'https://api.instagram.com/v1/users/self/media/recent/'
+github_json_file = 'https://api.github.com/repos/dhertz/dhertz.com/contents/static/photos.json'
 
 def main():
-  if len(sys.argv) != 2:
-    sys.exit("Please provide the instagram secret for " +
-      "the account you wish to use")
+  parser = ArgumentParser()
+  parser.add_argument("--insta-secret",
+    help="the instagram secret for your account", required=True)
+  parser.add_argument("--github-secret",
+    help="the github secret for your account", required=True)
 
-  instagram_secret = sys.argv[1]
-  images = get_my_media(instagram_secret)
-  all_images = add_images(instagram_secret, images, [])
-  print all_images
+  args = parser.parse_args()
+
+  images = get_my_media(args.insta_secret)
+
+  contents_file = get_file_contents_github(args.github_secret)
+  file_sha = contents_file['sha']
+
+  #Decode the file from basse64 and load the json.
+  existing_images = loads(b64decode(contents_file['content']))
+
+  all_images = add_images(args.insta_secret, images, existing_images)
+  update_file_github(args.github_secret, file_sha, all_images)
+
+def get_file_contents_github(secret):
+  # Get previous contents of file, and its SHA, so we can update it if need be
+  req = get(github_json_file, params={'access_token':secret})
+  if not req.status_code == 200:
+    print("Could not retrive original file from GitHub")
+    req.raise_for_status()
+  return req.json()
+
+def update_file_github(secret, file_sha, data):
+  #PUT the new conent on github
+  new_commit = {'message': 'Adding new instagram photo to json',
+                'author': {
+                  'name': "Daniel's Raspbery Pi",
+                  'email': 'git@dhertz.com'
+                },
+                'content':b64encode(dumps(data)),
+                'sha':file_sha
+                }
+  print new_commit
+  req = put(github_json_file, params={'access_token':secret},
+    data=dumps(new_commit))
+  if not req.status_code == 200:
+    print("Could not update file in GitHub")
+    req.raise_for_status()
 
 def add_images(instagram_secret, images, existing_images):
   ''' Haskell-inspired function that recurses over the list of images,
@@ -22,7 +59,7 @@ def add_images(instagram_secret, images, existing_images):
     the rest in reverse order. Also works when we start from scratch.
     I think this should be fine as we are usually adding a single image to
     the file, so we can escape after 2 calls to add_images().
-    
+
     TODO: make existing images ordered dict to to save iterating through it?
   '''
 
@@ -31,14 +68,13 @@ def add_images(instagram_secret, images, existing_images):
 
   if images[0]['id'] not in [image['id'] for image in existing_images]:
     if len(images) == 1:
-      images += get_my_media(intsagram_secret, images[0]['id'])
-    existing_images = add_images(intsagram_secret, images[1:], existing_images)
-    existing_images += images
-  return existing_images
+      images += get_my_media(instagram_secret, images[0]['id'])
+    images = [images[0]] + add_images(instagram_secret, images[1:], existing_images)
+  return images
 
 def get_my_media(secret, max_id=""):
   # Instagram paginates it's media, so we might have to make multiple requests
-  req = requests.get(insta_self_recent, params={'access_token':secret, 'max_id':max_id})
+  req = get(insta_self_recent, params={'access_token':secret, 'max_id':max_id})
   if req.status_code == 200 and req.json()['meta']['code'] == 200:
     resp = req.json()
     images = [{
