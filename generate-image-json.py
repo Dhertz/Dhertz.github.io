d0497f4a65a0ccb8d852bc75f674eda3c702f18e
Daniel Hertz <daniel@dhertz.com>
2016-05-16 23:13:39 +0100

    refactor to add instagram class and arg parse to main

--- generate-image-json.py
+++ generate-image-json.py
@@ -8,16 +8,9 @@ from requests import get, put
 insta_self_recent = 'https://api.instagram.com/v1/users/self/media/recent/'
 github_json_file = 'https://api.github.com/repos/dhertz/dhertz.com/contents/static/photos.json'
 
-def main():
-  parser = ArgumentParser()
-  parser.add_argument("--insta-secret",
-    help="the instagram secret for your account", required=True)
-  parser.add_argument("--github-secret",
-    help="the github secret for your account", required=True)
-
-  args = parser.parse_args()
-
-  images = get_my_media(args.insta_secret)
+def main(args):
+  instagram = Instagram(args.insta_secret)
+  images = instagram.get_my_media()
 
   contents_file = get_file_contents_github(args.github_secret)
   file_sha = contents_file['sha']
@@ -25,10 +18,10 @@ def main():
   #Decode the file from basse64 and load the json.
   existing_images = loads(b64decode(contents_file['content']))
 
-  new_images = add_images(args.insta_secret, images, existing_images)
+  new_images = instagram.add_images(images, existing_images)
   if new_images:
     print("Commiting new file to github")
-    update_file_github(args.github_secret, file_sha, new_images + existing_images)
+    #update_file_github(args.github_secret, file_sha, new_images + existing_images)
 
 def get_file_contents_github(secret):
   # Get previous contents of file, and its SHA, so we can update it if need be
@@ -54,49 +47,58 @@ def update_file_github(secret, file_sha, data):
     print("Could not update file in GitHub")
     req.raise_for_status()
 
-def add_images(instagram_secret, images, existing_images):
-  ''' Haskell-inspired function that recurses over the list of images,
-    finding the first image that we already have in our repo, then adding
-    the rest in reverse order. Also works when we start from scratch.
-    I think this should be fine as we are usually adding a single image to
-    the file, so we can escape after 2 calls to add_images().
+class Instagram:
+  def __init__(self, secret):
+    self.secret = secret
 
-    TODO: make existing images ordered dict to to save iterating through it?
-  '''
+  def add_images(self, images, existing_images):
+    '''Function that recurses over the list of images,
+      finding the first image that we already have in our repo, then adding
+      the rest in reverse order. Also works when we start from scratch.
 
-  if len(images) == 0:
-    return []
+      TODO: make existing images ordered dict to to save iterating through it?
+    '''
 
-  if images[0]['id'] not in [image['id'] for image in existing_images]:
-    if len(images) == 1:
-      images += get_my_media(instagram_secret, images[0]['id'])
-    images = [images[0]] + add_images(instagram_secret, images[1:], existing_images)
-    return images
-  else:
-    return []
+    if len(images) == 0:
+      return []
 
-def get_my_media(secret, max_id=""):
-  # Instagram paginates it's media, so we might have to make multiple requests
-  req = get(insta_self_recent, params={'access_token':secret, 'max_id':max_id})
-  if req.status_code == 200 and req.json()['meta']['code'] == 200:
-    resp = req.json()
-    images = []
-    for m in resp['data']:
-      title = m['caption']['text'] if m['caption'] else ''
-      images.append({
-                  'link':m['link'],
-                  'large':m['images']['low_resolution']['url'],
-                  'large_w':m['images']['low_resolution']['width'],
-                  'small':m['images']['thumbnail']['url'],
-                  'small_w':m['images']['thumbnail']['width'],
-                  'id':m['id'],
-                  'title':title
-                })
-    print req.url
-    return images
-  else:
-    print("Instagram API request failed!")
-    req.raise_for_status()
+    if images[0]['id'] not in [image['id'] for image in existing_images]:
+      if len(images) == 1:
+        images += self.get_my_media(images[0]['id'])
+      images = [images[0]] + self.add_images(images[1:])
+      return images
+    else:
+      return []
+
+  def get_my_media(self, max_id=""):
+    # Instagram paginates it's media, so we might have to make multiple requests
+    req = get(insta_self_recent, params={'access_token':self.secret, 'max_id':max_id})
+    if req.status_code == 200 and req.json()['meta']['code'] == 200:
+      resp = req.json()
+      images = []
+      for m in resp['data']:
+        title = m['caption']['text'] if m['caption'] else ''
+        images.append({
+                    'link':m['link'],
+                    'large':m['images']['low_resolution']['url'],
+                    'large_w':m['images']['low_resolution']['width'],
+                    'small':m['images']['thumbnail']['url'],
+                    'small_w':m['images']['thumbnail']['width'],
+                    'id':m['id'],
+                    'title':title
+                  })
+      print req.url
+      return images
+    else:
+      print("Instagram API request failed!")
+      req.raise_for_status()
 
 if __name__ == '__main__':
-  main()
+  parser = ArgumentParser()
+  parser.add_argument("--insta-secret",
+    help="the instagram secret for your account", required=True)
+  parser.add_argument("--github-secret",
+    help="the github secret for your account", required=True)
+
+  args = parser.parse_args()
+  main(args)
